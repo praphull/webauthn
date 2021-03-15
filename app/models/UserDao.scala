107e23d3e5aa9e48d7cd94dc427c28cf3c47a9f5
Praphull <1288395+praphull@users.noreply.github.com>
2021-03-15 20:13:03 +0530

    Change DAO to have a map of users

--- app/models/UserDao.scala
+++ app/models/UserDao.scala
@@ -4,12 +4,14 @@ import javax.inject.Inject
 
 @javax.inject.Singleton
 class UserDao @Inject()() {
+  private val users = Map(
+    "foo" -> "bar",
+    "foo1" -> "bar"
+  )
 
-    def lookupUser(u: User): Boolean = {
-        //TODO query your database here
-        if (u.username == "foo" && u.password == "foo") true else false
-    }
+  def lookupUser(u: User): Boolean = {
+    //TODO query your database here
+    users.get(u.username).contains(u.password)
+  }
 
 }
-
-
